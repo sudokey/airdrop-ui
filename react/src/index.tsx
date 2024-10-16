816318875700e57216eb105dfcaf277638f04a10
Pavel Ivanov <sudokey@gmail.com>
2024-10-16 09:36:52 +0300

    React example

---
+++ react/src/index.tsx
@@ -0,0 +1,171 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import {TonConnectUI, THEME, CHAIN} from '@tonconnect/ui'
+import {Clock, EverscaleStandaloneClient} from 'everscale-standalone-client'
+import {ProviderRpcClient} from 'everscale-inpage-provider'
+import {Address as TonAddress} from '@ton/core'
+import { AirdropUI } from '@broxus/airdrop-ui'
+import './index.css'
+
+const clock = new Clock()
+
+const rpc = new ProviderRpcClient({
+    fallback: () => EverscaleStandaloneClient.create({
+        clock,
+        connection: {
+            data: {
+                endpoint: 'https://jrpc-ton.broxus.com/rpc',
+            },
+            id: Number(CHAIN.MAINNET),
+            type: 'jrpc',
+        },
+    }),
+    forceUseFallback: true,
+})
+
+const tonConnectUI = new TonConnectUI({
+    manifestUrl: 'https://raw.githubusercontent.com/ton-connect/demo-dapp-with-wallet/master/public/tonconnect-manifest.json',
+    uiPreferences: {
+        theme: THEME.LIGHT,
+    },
+})
+
+const airdropUI = new AirdropUI(tonConnectUI, rpc, clock)
+
+export const App = () => {
+    const [state, setState] = React.useState(() => airdropUI.getState())
+
+    const connect = () => {
+        try {
+            tonConnectUI.openModal()
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    const disconnect = () => {
+        try {
+            tonConnectUI.disconnect()
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    React.useEffect(() => {
+        airdropUI.setAirdropName('test1')
+        return airdropUI.subscribe(setState)
+    }, [])
+
+    return (
+        <div>
+            <h1>TON Claim</h1>
+
+            <div className="grid">
+                <div>Wallet</div>
+                <div>
+                    {state.userAddress ? (
+                        <button onClick={disconnect}>Disconnect</button>
+                    ) : (
+                        <button onClick={connect}>Connect</button>
+                    )}
+                </div>
+
+                <div>Airdrop</div>
+                <div>
+                    <select
+                        value={state.airdropName ?? undefined}
+                        onChange={e => airdropUI.setAirdropName(e.target.value)}
+                    >
+                        <option></option>
+                        <option value="test1">Test1</option>
+                        <option value="test2">Test2</option>
+                        <option value="test3">Test3</option>
+                        <option value="test4">Test4</option>
+                        <option value="test5">Test5</option>
+                        <option value="test6">Test6</option>
+                        <option value="test7">Test7</option>
+                        <option value="test8">Test8</option>
+                        <option value="test9">Test9</option>
+                        <option value="test10">Test10</option>
+                    </select>
+                </div>
+
+                <div>Status</div>
+
+                {!state.userAddress ? (
+                    <div>Connect wallet</div>
+                ) : state.claimed === true ? (
+                    <div>Reward claimed</div>
+                ) : state.claimed === false && state.claimData?.status === 'signed' && state.storageKey ? (
+                    <div>
+                        <button
+                            disabled={state.loading || state.claimLoading}
+                            onClick={() => airdropUI.claim(state.claimData!, state.storageKey!)}
+                        >
+                            {(state.loading || state.claimLoading) ? 'Claim...' : 'Claim'}
+                        </button>
+                    </div>
+                ) : state.claimData?.status === 'inQueue' ? (
+                    <div>In queue</div>
+                ) : state.claimData?.status === 'noReward' ? (
+                    <div>No reward</div>
+                ) : !state.airdropName ? (
+                    <div>Select airdrop</div>
+                ) : (
+                    <div>Loading...</div>
+                )}
+
+                {state.userAddress && (
+                    <>
+                        <h3>User address</h3>
+                        <div>Raw address</div>
+                        <div>{state.userAddress}</div>
+                        <div>Bounceable</div>
+                        <div>{TonAddress.parseRaw(state.userAddress).toString()}</div>
+                        <div>Non bounceable</div>
+                        <div>{TonAddress.parseRaw(state.userAddress).toString({ bounceable: false })}</div>
+                    </>
+                )}
+
+                {state.claimData && (
+                    <>
+                        <h3>Claim data</h3>
+                        {state.claimData?.status === 'signed' ? (
+                            <>
+                                <div>Status</div>
+                                <div>{state.claimData.status}</div>
+                                <div>Factory address</div>
+                                <div>{state.claimData.factory}</div>
+                                <div>Airdrop</div>
+                                <div>{state.claimData.airdrop}</div>
+                                <div>Data</div>
+                                <div>{state.claimData.data}</div>
+                                <div>Signature</div>
+                                <div>{state.claimData.signature}</div>
+                                <div>Reward</div>
+                                <div>{state.claimData.reward}</div>
+                                <div>Nonce</div>
+                                <div>{state.claimData.nonce}</div>
+                            </>
+                        ) : state.claimData?.status === 'inQueue' ? (
+                            <>
+                                <div>Status</div>
+                                <div>{state.claimData.status}</div>
+                                <div>Place</div>
+                                <div>{state.claimData.place}</div>
+                            </>
+                        ) : state.claimData?.status === 'noReward' ? (
+                            <>
+                                <div>Status</div>
+                                <div>{state.claimData.status}</div>
+                            </>
+                        ) : null}
+                    </>
+                )}
+            </div>
+        </div>
+    )
+}
+
+const root = createRoot(document.getElementById('root')!)
+root.render(<App />)
